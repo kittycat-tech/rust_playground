4a1e2a0fdbf1879e2b40e5e4a243fee3118de957
Erica Madebeykin <erica.madebeykina@gmail.com>
2025-07-04 21:08:30 +0400

    My First loop and inequalities (Ordering) in Rust yey!

---
+++ secret_number_game/target/debug/build/libc-9eb3e36030d8a177/build_script_build-9eb3e36030d8a177.d
@@ -0,0 +1,5 @@
+/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/build/libc-9eb3e36030d8a177/build_script_build-9eb3e36030d8a177.d: /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs
+
+/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/build/libc-9eb3e36030d8a177/build_script_build-9eb3e36030d8a177: /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs
+
+/Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs:
---
+++ secret_number_game/target/debug/deps/rand_chacha-5c9948c1682b9464.d
@@ -0,0 +1,9 @@
+/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/deps/rand_chacha-5c9948c1682b9464.d: /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/deps/librand_chacha-5c9948c1682b9464.rlib: /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/deps/librand_chacha-5c9948c1682b9464.rmeta: /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
+/Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
+/Users/ericamadebeykin/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
