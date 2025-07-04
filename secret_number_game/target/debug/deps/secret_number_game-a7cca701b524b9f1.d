5345c5b3923b15ad2651f61b2feae2a148afe860
Erica Madebeykin <erica.madebeykina@gmail.com>
2025-07-04 21:35:23 +0400

    cargo clean

--- secret_number_game/target/debug/deps/secret_number_game-a7cca701b524b9f1.d
+++
@@ -1,5 +0,0 @@
-/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/deps/secret_number_game-a7cca701b524b9f1.d: src/main.rs
-
-/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/deps/secret_number_game-a7cca701b524b9f1: src/main.rs
-
-src/main.rs:
--- secret_number_game/target/debug/secret_number_game.d
+++
@@ -1,1 +0,0 @@
-/Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/target/debug/secret_number_game: /Users/ericamadebeykin/dev/rust/rust_playground/secret_number_game/src/main.rs
