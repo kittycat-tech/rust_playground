a5c492047881fcd33b41723c34b5b0639a8765a7
Erica Madebeykin <erica.madebeykina@gmail.com>
2025-07-04 18:03:58 +0400

    first practice on standard input output (std::io)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is a guessing game");
+    println!("Guess the number");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("You failed to guess the number");
+
+    println!("You guessed: {guess}");
 }
